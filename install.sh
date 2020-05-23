e52048932165ef027789f37f8c948d589cb63939
slidinghotdog <pexis@email.com>
2020-05-23 18:34:53 -0300

    testing

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 #PARTITION DISK BEFORE USING THIS SCRIPT
 # list devices: lsblk
-# partition disk: cfdisk <device>
+# partition disk: cfdisk -z <device>
 
-#-----------------------CONFIG--------------------------------------
-#device for MBR, comment for uefi
+#--------------------------------------CONFIG--------------------------------------
+#device for MBR, comment for UEFI (also set DEV_BOOT for UEFI)
 DEV_MBR=/dev/sda
 
 #select partitions to use, comment if not using
@@ -13,6 +13,9 @@ DEV_MBR=/dev/sda
 DEV_SWAP=/dev/sda1
 DEV_ROOT=/dev/sda2
 #DEV_HOME=/dev/sda3
+
+#uncomment to skip formatting (need to format manually if skipped)
+#SKIP_FORMAT=true
 
 ROOT_PWD=root
 USER=user
@@ -30,46 +33,69 @@ HOSTS="127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t${HOSTNAME}.localdomai
 
 PACKAGES="base linux nano grub networkmanager sudo"
 
-#Services to enable, eg: "NetworkManager lightdm"
+#Services to enable (space-separated eg: "NetworkManager lightdm")
 SERVICES="NetworkManager"
 
-#-----------------------END CONFIG----------------------------------
+#----------------------------------END CONFIG----------------------------------
 
 if [ -z "$DEV_MBR" ]; then
     PACKAGES+=" efibootmgr"
 fi
 
 if [ -n "$DEV_SWAP" ]; then
+    if [ -z "$SKIP_FORMAT" ]; then
+        yes | mkswap -L swap "$DEV_SWAP"
+    fi
     echo enabling swap on "$DEV_SWAP"    
     swapon "$DEV_SWAP"
 fi
+
 if [ -n "$DEV_ROOT" ]; then
+    if [ -z "$SKIP_FORMAT" ]; then
+        yes | mkfs.ext4 -L root "$DEV_ROOT"
+    fi
     echo mounting "$DEV_ROOT" to /mnt    
     mount "$DEV_ROOT" /mnt
 fi
+
 if [ -z "$DEV_MBR" ]; then
+    if [ -z "$SKIP_FORMAT" ]; then
+        yes | mkfs.fat -F32 "$DEV_BOOT"
+    fi
     echo mounting "$DEV_BOOT" to /mnt/boot/efi
     mkdir -p /mnt/boot/efi
     mount "$DEV_BOOT" /mnt/boot/efi
 fi
+
 if [ -n "$DEV_HOME" ]; then
+    if [ -z "$SKIP_FORMAT" ]; then
+        yes | mkfs.ext4 -L home "$DEV_HOME"
+    fi
     echo mounting "$DEV_HOME" to /mnt/home
     mkdir -p /mnt/home    
     mount "$DEV_HOME" /mnt/home
 fi
 
+sleep 2
+
 pacstrap /mnt $PACKAGES
 genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt hwclock --systohc --utc
+arch-chroot /mnt timedatectl set-ntp true
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/"${TIMEZONE}" /etc/localtime
+
 echo -e "$HOSTS" >> /mnt/etc/hosts
-echo $HOSTNAME > /mnt/etc/hostname
-echo KEYMAP=$KEYMAP >> /mnt/etc/vconsole.conf
+echo "$HOSTNAME" > /mnt/etc/hostname
+echo "KEYMAP=$KEYMAP" >> /mnt/etc/vconsole.conf
 echo -e "$LOCALE_GEN" >> /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
-echo LANG=$LANG >> /mnt/etc/locale.conf
-echo "%wheel ALL=(ALL) ALL" > /mnt/etc/sudoers.d/wheel
-arch-chroot /mnt useradd -aG wheel $USER
-echo "$USER":"$USER_PWD" | chpasswd --root /mnt
+echo "LANG=$LANG" >> /mnt/etc/locale.conf
+
+arch-chroot /mnt useradd -mG wheel $USER
+echo "$USER:$USER_PWD" | chpasswd --root /mnt
 echo "root:$ROOT_PWD" | chpasswd --root /mnt
+echo "%wheel ALL=(ALL) ALL" > /mnt/etc/sudoers.d/wheel
 
 if [ -n "$DEV_MBR" ]; then
     arch-chroot /mnt grub-install $DEV_MBR
